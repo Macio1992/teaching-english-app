9bb96e7a3537d40897a0d6e36c30486f88c95519
MaciejZbierowski <maix19bep@gmail.com>
2016-06-26 17:02:53 +0200

    is trying connecting to MongoDB

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb://mongouser:mongopassword@ds041992.mongolab.com:41992/appdb'
+};
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var port = process.env.PORT || 8080;
+
+var configDB = require('./config/database');
+mongoose.connect(configDB.url);
+
+mongoose.connection.on('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', function(){
+    console.err.bind(console, 'MongoDB Error');
+});
 
 /*app.get('/', function(req, res){
     res.send({message: 'hooray! welcome to our app'});
